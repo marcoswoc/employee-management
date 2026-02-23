b2a0a70651aa091f8d00bb4a8b5aeb7f6c2847aa
marcos wellington oliveira costa <marcoswoc@gmail.com>
2026-02-23 20:46:52 -0300

    Adicionando Value Objects

---
+++ EmployeeManagement.Domain/ValueObjects/Cpf.cs
@@ -0,0 +1,19 @@
+﻿namespace EmployeeManagement.Domain.ValueObjects;
+
+public class Cpf
+{
+    public string Value { get; }
+
+    public Cpf(string value)
+    {
+        if (!IsValid(value))        
+            throw new ArgumentException("CPF inválido");
+        
+
+        Value = value[..'.'][..'-'];
+    }
+
+    private static bool IsValid(string cpf) => !string.IsNullOrWhiteSpace(cpf) && cpf.Length >= 11;
+
+    public override string ToString() => Value;
+}
