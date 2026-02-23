0d5009727ee1bc7b394d0350d5fde0a4be42d285
marcos wellington oliveira costa <marcoswoc@gmail.com>
2026-02-23 20:29:41 -0300

    Adicionando entidades

---
+++ EmployeeManagement.Domain/Entities/AuditLog.cs
@@ -0,0 +1,14 @@
+﻿namespace EmployeeManagement.Domain.Entities;
+
+public class AuditLog
+{
+    public Guid Id { get; set; }
+    public required string EntityName { get; set; }
+    public Guid EntityId { get; set; }
+    public required string Action { get; set; } 
+    public string? OldValues { get; set; } 
+    public required string NewValues { get; set; } 
+    public DateTime CreatedAt { get; set; }
+    public Guid CreatedBy { get; set; }
+    public required string CreatedByName { get; set; }
+}
---
+++ EmployeeManagement.Domain/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeManagement.Domain.Entities;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public required string Email { get; set; }
+    public required string PasswordHash { get; set; }
+    public bool IsActive { get; set; }
+    public string[] Roles { get; set; } = ["User"];
+    public DateTime CreatedAt { get; set; }
+    public DateTime LastLogin { get; set; }
+}
